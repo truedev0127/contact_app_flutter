3b50380626121c409042ce13da8b7d65cb9d7264
truedev0127 <kevinanderson.tech0127@gmail.com>
2026-01-19 10:03:26 +0900

    complete statelesswidget

---
+++ lib/data/contact.dart
@@ -0,0 +1,8 @@
+class Contact {
+  String name;
+  String email;
+  // +1234567890 - not all phone bumbers are valid mathematical numbers
+  String phoneNumber;
+
+  Contact({required this.name, required this.email, required this.phoneNumber});
+}
---
+++ lib/ui/contacts_list/contacts_list_page.dart
@@ -0,0 +1,39 @@
+import 'package:contact_app/data/contact.dart';
+import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
+
+class ContactsListPage extends StatefulWidget {
+  @override
+  State<ContactsListPage> createState() => _ContactsListPageState();
+}
+
+class _ContactsListPageState extends State<ContactsListPage> {
+  List<Contact> _contacts = List.generate(50, (index) {
+    return Contact(
+      name: Faker().person.name(),
+      email: Faker().internet.email(),
+      phoneNumber: Faker().phoneNumber.us(),
+    );
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contacts', style: TextStyle(fontSize: 35)),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: ListView.builder(
+        itemCount: _contacts.length,
+        // Runs and Builds every single item list item
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_contacts[index].name),
+            subtitle: Text(_contacts[index].email),
+          );
+        },
+      ),
+    );
+  }
+}
